2de32d59e657c50e9c7395cac74594752b95a634
Wotori <10486621+wotori@users.noreply.github.com>
2022-12-14 21:03:55 +0300

    reset counter on close modal

--- frontend/src/components/modal/index.jsx
+++ frontend/src/components/modal/index.jsx
@@ -4,6 +4,10 @@ export function Modal(props) {
   let { show, setShow } = props;
   const [count, setCount] = useState(0);
   let limit = 100;
+  function reset() {
+    setCount(0);
+    setShow(false);
+  }
   return (
     <>
       {show ? (
@@ -15,7 +19,9 @@ export function Modal(props) {
             onClick={(e) => {
               let check = e.target.className.slice(0, 3);
               console.log("click");
-              if (check == "xyz") setShow(false);
+              if (check == "xyz") {
+                reset();
+              }
             }}
           >
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
@@ -54,14 +60,14 @@ export function Modal(props) {
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={() => setShow(false)}
+                    onClick={() => reset()}
                   >
                     Close
                   </button>
                   <button
                     className="bg-blue text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={() => setShow(false)}
+                    onClick={() => reset()}
                   >
                     publish
                   </button>
