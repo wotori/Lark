88222389cceec1222d304a84e3efb20d4bdd0d8b
Wotori <10486621+wotori@users.noreply.github.com>
2022-12-15 14:35:11 +0300

    polish css

--- frontend/src/components/textbox/index.jsx
+++ frontend/src/components/textbox/index.jsx
@@ -1,17 +1,19 @@
 export function LarkBox(props) {
-  let { text, userName, date } = props;
+  let { text, userName, date, hashtags } = props;
   return (
-    <div className="">
+    <div className="bg-sky-50 m-3 rounded-xl">
       {/* Author */}
-      <div className="flex flex-row items-center mt-5 mb-3">
-        <img className="h-7 w-7 mx-3" src="svg/user.svg"></img>
-        <p className="text-sm">
-          {userName ? userName : "Mina wallet address"}
-        </p>
+      <div className="flex flex-row justify-around pr-5">
+        <div className="flex flex-row items-center mt-5 mb-3 self-center">
+          <img className="h-7 w-7 mx-3" src="svg/user.svg"></img>
+          <p className="text-sm">
+            {userName ? userName : "Mina wallet address"}
+          </p>
+        </div>
       </div>
       {/* Content */}
       <div
-        className="p-2 min-w-1 max-w-xs bg-white rounded-xl shadow-lg text-blue 
+        className="p-2 m-2 min-w-1 max-w-xs bg-white rounded-xl shadow-lg text-blue 
         outline outline-offset-2 outline-blue-500"
       >
         <p>
@@ -19,8 +21,13 @@ export function LarkBox(props) {
           understand this lark, but I love it.
         </p>
       </div>
-      {/* Date */}
-      <p className="m-1 text-sm">{date ? date : "27.11.2022"}</p>
+      {/* Date & hashtags */}
+      <div className="pt-3">
+        <p className="m-1 text-sm text-center">{date ? date : "undefined"}</p>
+        <p className="m-1 text-sm text-center">
+          {hashtags ? hashtags : "#undefined"}
+        </p>
+      </div>
     </div>
   );
 }
